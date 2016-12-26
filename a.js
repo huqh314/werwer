e3da572e8b05914e4326339a7200b6d43dbdb806
huqh <huqh@asiainfo.com>
2016-12-26 09:15:12 +0800

    sdfsa

---
+++ a.js
@@ -0,0 +1,15 @@
+﻿function f1(){
+　　　　var n=999;
+        nAdd=function(){
+           n+=1;
+		   console.log("调用了nadd");
+		}
+　　　　function f2(){
+　　　　　　console.log(n);
+　　　　}
+　　　　return f2;
+　　}
+　　var result=f1();
+　//　result(); // 999
+　　nAdd();
+　//　result(); // 1000 i write something suibianly
